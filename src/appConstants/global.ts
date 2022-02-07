933c25a9a275a3057699a523e5161773a0541afa
Alexey Shevchenko <kamikozz@mail.ru>
2022-02-07 14:58:01 +0300

    refactor: replace RESERVED_ADDRESSES with constant from appConstants

--- src/appConstants/global.ts
+++ src/appConstants/global.ts
@@ -1,1 +1,1 @@
-export const TOKEN_ADDRESSES_MAX_COUNT = 4; // to be added to transfer or etc in contracts
+export const TOKEN_ADDRESSES_MAX_COUNT = 4; // supported only 4 tokens to be added to transfer or reserved address in contracts
