2a272fafebf0eaf4bf1d3fe96914a2bf1b451cc2
Alexey Shevchenko <kamikozz@mail.ru>
2022-02-16 14:39:55 +0300

    feat(cb-118): open connect dropdown modal if user is unauthorized, but CreateContract form is valid

---
+++ src/components/ConnectDropdownModal/ConnectDropdownModal.mock.tsx
@@ -0,0 +1,7 @@
+import { ConnectDropdownModalProps } from './ConnectDropdownModal';
+
+export const connectDropdownModalPropsMocked: ConnectDropdownModalProps = {
+  open: true,
+  onClose: () => { },
+  address: '',
+};
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useShallowSelector } from './useShallowSelector';
 export * from './useNavigation';
 export * from './useWindowState';
+export * from './modals';
