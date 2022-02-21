b9db85e26f6a703a2e59c9c2d5fae03fcc2c0037
Valentin Kulchikhin <valkulchikhin@gmail.com>
2022-02-21 15:22:04 +0300

    fix celo connect

--- src/services/walletConnect/index.tsx
+++ src/services/walletConnect/index.tsx
@@ -83,6 +83,8 @@ any,
             wallet: provider,
           });
         }
+
+        return;
       } catch (err) {
         console.log(err);
         this.disconnect();
@@ -91,19 +93,19 @@ any,
           message: `${provider} is not installed or unlocked`,
         });
       }
-    } else if (window.celo) {
-      await window.celo.enable();
-      if (!window.celo.selectedAddress) {
-        setNotification({
-          type: 'error',
-          message: 'Celo extension wallet is not installed or unlocked',
-        });
+    }
 
-        return;
-      }
+    if (window.celo) {
+      await window.celo.enable();
+
       this.props.connectWallet({
         address: window.celo.selectedAddress,
         wallet: 'celo',
+      });
+    } else {
+      setNotification({
+        type: 'error',
+        message: 'Celo extension wallet is not installed or unlocked',
       });
     }
   };
