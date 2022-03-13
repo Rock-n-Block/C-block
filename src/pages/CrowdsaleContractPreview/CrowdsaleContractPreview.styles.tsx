42f26dbe91d09c4ca5a31f2fe06f07ebe4144c44
Alexey Shevchenko <kamikozz@mail.ru>
2022-03-13 22:15:05 +0300

    fix(theme/light): colors for textfields

--- src/pages/CrowdsaleContractPreview/CrowdsaleContractPreview.styles.tsx
+++ src/pages/CrowdsaleContractPreview/CrowdsaleContractPreview.styles.tsx
@@ -3,14 +3,14 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 import {
   COLOR_ACID_GREEN,
   COLOR_BLACK_2,
-  COLOR_GREY_3,
+  COLOR_GREY_4,
 } from 'theme/colors';
 import { baseFieldWidthRestriction, getBorderStyle, separator } from 'utils';
 
 export const useStyles = makeStyles((theme: Theme) => createStyles({
   mixedSection: {
     border: getBorderStyle(theme),
-    background: theme.palette.type === 'dark' ? COLOR_BLACK_2 : COLOR_GREY_3,
+    background: theme.palette.type === 'dark' ? COLOR_BLACK_2 : COLOR_GREY_4,
   },
   tokenContractInfoBlock: {
     padding: theme.spacing(1.5, 1.25),
